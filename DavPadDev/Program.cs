98e31944c149015a663037f6b454d838b9db9981
dav.pad <dav.pad@davpaddev.com>
2025-09-22 19:30:04 -0500

    Added Modal, Updated Homepage

--- DavPadDev/Program.cs
+++ DavPadDev/Program.cs
@@ -1,10 +1,13 @@
 using DavPadDev;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using DavPadDev.Services;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSingleton<ModalService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
---
+++ DavPadDev/Services/ModalService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DavPadDev.Services
+{
+    public class ModalService
+    {
+        public string Title { get; private set; } = string.Empty;
+        public string Message { get; private set; } = string.Empty;
+        public string Note { get; private set; } = string.Empty;
+        public List<ModalButton> Buttons { get; private set; } = new();
+
+        public bool Show { get; private set; }
+
+        public event Action OnChange;
+
+        public void ShowModal(string title, string message, string note = "", params ModalButton[] buttons)
+        {
+            Title = title;
+            Message = message;
+            Note = note;
+            Buttons = new List<ModalButton>(buttons);
+            Show = true;
+            NotifyStateChanged();
+        }
+
+        public void CloseModal()
+        {
+            Show = false;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+
+    public class ModalButton
+    {
+        public string Text { get; set; }
+        public Action OnClick { get; set; }
+        public string CssClass { get; set; } = "btn";
+
+        public ModalButton(string text, Action onClick, string cssClass = "btn")
+        {
+            Text = text;
+            OnClick = onClick;
+            CssClass = cssClass;
+        }
+    }
+}

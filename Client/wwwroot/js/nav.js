f1c6c784f47745f09081ee2affa6bc44d62826da
dav.pad <dav.pad@davpaddev.com>
2025-12-01 16:29:29 -0600

    Add global design tokens and fully polish Contact page UI (scoped CSS + animations)

---
+++ Client/wwwroot/js/nav.js
@@ -0,0 +1,14 @@
+﻿let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+    const current = window.scrollY;
+
+    // collapse when scrolling down more than ~10px
+    if (current > lastScroll + 10) {
+        try {
+            DotNet.invokeMethodAsync("DavPadDev.Client", "CollapseFromScroll");
+        } catch { }
+    }
+
+    lastScroll = current;
+});
